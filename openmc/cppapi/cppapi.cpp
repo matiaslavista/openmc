46ac2553598f8fa0f7eab228bc24872eea04e9b4
lavistam <20384517+matiaslavista@users.noreply.github.com>
2019-04-11 12:39:32 -0500

    Paul review changes, plus setup.py generic for openmc_cpp

--- openmc/cppapi/cppapi.cpp
+++ openmc/cppapi/cppapi.cpp
@@ -1,32 +1,34 @@
+
+#include "openmc/capi.h"
+
 #include <pybind11/pybind11.h>
-#include "openmc/capi.h" 
+#include <pybind11/stl.h>
+
 #include <string>
 #include <vector>
-#include <pybind11/stl.h>
 #include <algorithm>
 namespace py = pybind11;
 
-
-namespace openmc {
+namespace openmc
+{
 
 using std::string;
 using std::vector;
 
-int init(vector<string> argv_c = {}, const void* intracomm = NULL) {
-    vector<char*> argv_char(argv_c.size());
-    std::transform(argv_c.begin(), argv_c.end(), argv_char.begin(),
-            [](string& a){return const_cast<char *>(a.c_str());} );
-    return openmc_init(argv_char.size(), argv_char.data(), intracomm);
-}
-        
+int init(vector<string> argv = {}, const void *intracomm = NULL)
+{
+  vector<char *> argv_char(argv.size());
+  std::transform(argv.begin(), argv.end(), argv_char.begin(),
+      [](string &a) { return const_cast<char*>(a.c_str()); });
+  return openmc_init(argv_char.size(), argv_char.data(), intracomm);
 }
 
-int add(int i, int j) {
-    return i + j;
-}
-PYBIND11_MODULE(openmc_cpp, m) {
-    m.doc() = R"pbdoc(
-        CPP API to OPENMCs
+} // namespace openmc
+
+PYBIND11_MODULE(openmc_cpp, m)
+{
+  m.doc() = R"pbdoc(
+        C++ API to OpenMC
         -----------------------
         .. currentmodule:: openmc_cpp
         .. autosummary::
@@ -34,13 +36,12 @@ PYBIND11_MODULE(openmc_cpp, m) {
            init
     )pbdoc";
 
-    m.def("init", &openmc::init, py::arg("argv_c") = py::list(), 
-            py::arg("intracomm") = NULL,
-          );
+  m.def("init", &openmc::init, py::arg("argv") = py::list(), 
+    py::arg("intracomm") = NULL);
 
 #ifdef VERSION_INFO
-    m.attr("__version__") = VERSION_INFO;
+  m.attr("__version__") = VERSION_INFO;
 #else
-    m.attr("__version__") = "dev";
+  m.attr("__version__") = "dev";
 #endif
 }
