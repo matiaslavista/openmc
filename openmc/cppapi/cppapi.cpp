c10609ff09970d9c0daf7a8da6bfaac6d2a8aa87
lavistam <20384517+matiaslavista@users.noreply.github.com>
2019-04-16 12:26:49 -0500

    style fixes fro openmc

--- openmc/cppapi/cppapi.cpp
+++ openmc/cppapi/cppapi.cpp
@@ -14,7 +14,8 @@ namespace openmc {
 using std::string;
 using std::vector;
 
-int init(vector<string> argv = {}, const void *intracomm = NULL) {
+int init(vector<string> argv = {}, const void *intracomm = NULL) 
+{
   vector<char *> argv_char(argv.size());
   std::transform(argv.begin(), argv.end(), argv_char.begin(),
                  [](string &a) { return const_cast<char *>(a.c_str()); });
@@ -27,7 +28,8 @@ int finalize() { return openmc_finalize(); }
 
 } // namespace openmc
 
-PYBIND11_MODULE(openmc_cpp, m) {
+PYBIND11_MODULE(openmc_cpp, m) 
+{
   m.doc() = R"pbdoc(
         C++ API to OpenMC
         -----------------------
