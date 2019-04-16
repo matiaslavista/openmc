01b6deba7494f1c0f9a5f379c2822ab6100f9316
lavistam <20384517+matiaslavista@users.noreply.github.com>
2019-04-16 12:20:27 -0500

    openmc style

--- openmc/cppapi/cppapi.cpp
+++ openmc/cppapi/cppapi.cpp
@@ -4,39 +4,30 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
+#include <algorithm>
 #include <string>
 #include <vector>
-#include <algorithm>
 namespace py = pybind11;
 
-namespace openmc
-{
+namespace openmc {
 
 using std::string;
 using std::vector;
 
-int init(vector<string> argv = {}, const void *intracomm = NULL)
-{
+int init(vector<string> argv = {}, const void *intracomm = NULL) {
   vector<char *> argv_char(argv.size());
   std::transform(argv.begin(), argv.end(), argv_char.begin(),
-      [](string &a) { return const_cast<char*>(a.c_str()); });
+                 [](string &a) { return const_cast<char *>(a.c_str()); });
   return openmc_init(argv_char.size(), argv_char.data(), intracomm);
 }
 
-int run()
-{
-  return openmc_run();
-}
+int run() { return openmc_run(); }
 
-int finalize()
-{
-    return openmc_finalize();
-}
+int finalize() { return openmc_finalize(); }
 
 } // namespace openmc
 
-PYBIND11_MODULE(openmc_cpp, m)
-{
+PYBIND11_MODULE(openmc_cpp, m) {
   m.doc() = R"pbdoc(
         C++ API to OpenMC
         -----------------------
@@ -46,9 +37,9 @@ PYBIND11_MODULE(openmc_cpp, m)
            init
     )pbdoc";
 
-  m.def("init", &openmc::init, py::arg("argv") = py::list(), 
-    py::arg("intracomm") = NULL);
-  m.def("run", &openmc::run); 
+  m.def("init", &openmc::init, py::arg("argv") = py::list(),
+        py::arg("intracomm") = NULL);
+  m.def("run", &openmc::run);
   m.def("finalize", &openmc::finalize);
 
 #ifdef VERSION_INFO
