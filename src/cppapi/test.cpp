fce98d92208eb301a31491efc73b34eec2b57c6b
lavistam <20384517+matiaslavista@users.noreply.github.com>
2019-04-25 11:12:39 -0500

    Spacing and comments

--- src/cppapi/test.cpp
+++ src/cppapi/test.cpp
@@ -2,17 +2,17 @@
 
 namespace py = pybind11;
 
-
+//Place holder functions
 int add(int i, int j) {
   return i + j;
 }
-
+//place holder functions
 int subtract(int i, int j) {
   return i-j;
 }
 
 namespace openmc {
-
+//adding them to the module from a diff file
 void init_test_file(py::module& m) {
   m.def("add", &add);
   m.def("subtract", &subtract);
