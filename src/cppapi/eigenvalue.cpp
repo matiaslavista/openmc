bacf200a00888ede03cc5a5c78ca6d02b4a3c0aa
lavistam <20384517+matiaslavista@users.noreply.github.com>
2019-05-09 14:07:23 -0500

    get_keff

---
+++ src/cppapi/eigenvalue.cpp
@@ -0,0 +1,27 @@
+
+#include "openmc/eigenvalue.h"
+#include "openmc/cppapi/eigenvalue.h"
+
+#include <tuple>
+#include <stdexcept>
+
+namespace openmc {
+
+namespace py = pybind11;
+
+std::tuple<double, double> get_keff(){
+  double arr[2];
+  int ret = openmc_get_keff(arr);
+  return std::make_tuple(arr[0],arr[1]);
+}
+
+//==============================================================================
+// CPP API
+//==============================================================================
+//Add elements of banks to the module
+void init_eigenvalue(py::module& m)
+{
+  m.def("get_keff", &openmc::get_keff);
+}
+
+}
